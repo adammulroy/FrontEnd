7cd9a21009659ae079c2258432a5a39cdc4b45f5
Adam Mulroy <adam.m.mulroy@gmail.com>
2015-04-11 18:34:41 -0500

    Added fresh_tomatoes.py from udacity.
    Called function from fresh_tomatoes that will build the html website

--- movie.py
+++ movie.py
@@ -1,4 +1,5 @@
 __author__ = 'adam.mulroy'
+import webbrowser
 
 
 class Movie():
@@ -9,4 +10,5 @@ class Movie():
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
-
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
--- movie_website.py
+++ movie_website.py
@@ -1,10 +1,22 @@
 __author__ = 'adam.mulroy'
 import movie
+import fresh_tomatoes
 
+toy_story = movie.Movie('Toy Story',
+                        'A boy and his toys that come to life',
+                        'http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg',
+                        'https://www.youtube.com/watch?v=KYz2wyBy3kc')
 
-toy_story = movie.Movie('Toy Story', 'A boy and his toys that come to life', 'Poster Link', 'Youtube link')
-avatar = movie.Movie('Avatar', 'A marine on an alien planet', 'Poster Link', 'Youtube link')
+avatar = movie.Movie('Avatar',
+                     'A marine on an alien planet',
+                     'http://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg',
+                     'https://www.youtube.com/watch?v=cRdxXPV9GNQ')
 
+king_kong = movie.Movie('King Kong',
+                       'A big ape and a babe',
+                       'http://upload.wikimedia.org/wikipedia/en/6/6a/Kingkong_bigfinal1.jpg',
+                       'https://www.youtube.com/watch?v=B5j_2sRUTbU')
 
-print(toy_story.title)
-print(avatar.storyline)
+movies = [toy_story, avatar, king_kong]
+
+fresh_tomatoes.open_movies_page(movies)
